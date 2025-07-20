e839ee43dcf1c889e1d445aec31241c532826fd1
jerryagenyi <jerryagenyi@gmail.com>
2025-07-20 18:31:50 +0100

    Move install-extensions.sh to .vscode for unified dev setup

---
+++ .vscode/install-extensions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+code --install-extension Vue.volar
+code --install-extension esbenp.prettier-vscode
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension github.vscode-pull-request-github
+code --install-extension rangav.vscode-thunder-client
+code --install-extension eamodio.gitlens
+code --install-extension streetsidesoftware.code-spell-checker
+
+echo "✅ SkilledUp Life VS Code extensions installed." 
